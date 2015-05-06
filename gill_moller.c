6c1937e93210f107c650d04eb4b4d4e1432977c1
Adrian Sadłocha <adrian@sadlocha.eu>
2015-05-06 23:15:20 +0200

    Move measuring code into its own function

--- gill_moller.c
+++ gill_moller.c
@@ -30,29 +30,30 @@ void print_csv_row(clock_t tic, clock_t toc, double result) {
     printf("%f,%f,", (double)(toc - tic) / CLOCKS_PER_SEC, result);
 }
 
+void measure_function(double (*f)(long, double), long n) {
+    double a = 1.0/3.0, result;
+    clock_t tic, toc;
+
+    tic = clock();
+    result = (*f)(n, a);
+    toc = clock();
+    print_csv_row(tic, toc, result);
+}
+
 int main(int argc, char **argv) {
     if(argc < 2 || !strcmp(argv[1], "--help")) {
         printf("Usage: %s NUM\n", argv[0]);
         return 1;
     }
 
-    double a = 1.0/3.0, result;
-    clock_t tic, toc;
     print_csv_header();
 
     for(int i = 0; i <= atoi(argv[1]); ++i) {
         printf("%d,", i);
         long n = 3 * pow(10, i);
 
-        tic = clock();
-        result = gill_moller(n, a);
-        toc = clock();
-        print_csv_row(tic, toc, result);
-
-        tic = clock();
-        result = naive(n, a);
-        toc = clock();
-        print_csv_row(tic, toc, result);
+        measure_function(gill_moller, n);
+        measure_function(naive, n);
 
         printf("\n");
     }
